a6e58bf69ee0df0a4efe2a214978043d0ee2d956
callor <callor@callor.com>
2024-05-17 14:18:59 +0900

    mermaid

--- openAI-001-UML/src/app/comps/ViewResult.js
+++ openAI-001-UML/src/app/comps/ViewResult.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCallback } from "react";
+import Mermaid from "../modules/Mermaid";
 
 const ViewResultPage = ({ resultText }) => {
   // 문자열 결과를 보기 좋게 편집하기
@@ -24,11 +25,35 @@ const ViewResultPage = ({ resultText }) => {
     return result.join("");
   });
 
+  const getMeraid = useCallback((text) => {
+    const textList = text.split("\n");
+    let flag = false;
+    const result = textList.map((t) => {
+      if (t.trim().startsWith("```mermaid")) {
+        flag = true;
+        return "";
+      } else if (flag && t.trim().startsWith("```")) {
+        flag = false;
+        return "\n\n";
+      } else if (flag) {
+        return `${t}\n`;
+      } else {
+        return "\n";
+      }
+    });
+    return result.join("\n");
+  });
+
   return (
-    <div
-      dangerouslySetInnerHTML={{ __html: textToHtml(resultText) }}
-    ></div>
+    <>
+      <div
+        dangerouslySetInnerHTML={{ __html: textToHtml(resultText) }}
+      ></div>
+      {resultText?.includes("```mermaid") && (
+        <Mermaid chart={getMeraid(resultText)} />
+      )}
+      <div>{getMeraid(resultText)}</div>
+    </>
   );
 };
-
 export default ViewResultPage;
